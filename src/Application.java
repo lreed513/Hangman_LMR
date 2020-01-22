302f99dad265a5f7b1aee6107696150d2a12c84b
Leaia M. Reed <lreed@bellarmine.edu>
2020-01-22 08:51:07 -0500

    first commit

---
+++ src/Application.java
@@ -0,0 +1,17 @@
+/**
+ * This class should instantiate a Hangman object that plays.
+ * @author lreed CS131
+ *Spring 2020
+ */
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("\nWould you like to play Y/N?");
+		System.out.println("You have 5 incorrect guesses left.");
+		
+		
+	}// end String
+
+}//end class
